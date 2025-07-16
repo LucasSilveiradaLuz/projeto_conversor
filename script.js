3032b8a1c3a9502845e6d486bcf9d32e7ede31ef
Lucas <luzlucas22022007@gmail.com>
2025-07-16 18:47:36 -0300

    Atualizando arquivos do projeto

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const temperatura =document.getElementById("temperatura")
+const graus =document.getElementById("graus")
 const button = document.getElementById("button")
 const resultado = document.getElementById("resultado")
 const temperaturas = document.getElementById("temperaturas")
 function converterTemperatura(){
 
-const number = parseFloat(temperatura.value);
+const number = parseFloat(graus.value);
 let temp = number
 let formula;
 if (temperaturas.value=== "fahrenheit"){
 formula = (temp * 9/5) + 32;
-resultado.innerText = "Resultado = " + formula.toFixed(2) + "°F"
+resultado.innerText = "Resultado = " + formula.toFixed(1) + "°F"
+
 }
 else {
     formula = (temp-32)*5/9
-    resultado.innerText = "Resultado = " + formula.toFixed(2) + "°C"
+    resultado.innerText = "Resultado = " + formula.toFixed(1) + "°C"
+  
 }
-console.log(temperaturas.value)
-console.log(temperaturas.value)
+
 }
